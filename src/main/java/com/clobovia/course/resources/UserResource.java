d0a005719f571a329ca3b5e225796a52505f481e
Carlota Lobo <carlota.lobo@viaconsultingway.com>
2025-01-27 16:51:40 +0000

    Service layer, component registration

--- src/main/java/com/clobovia/course/resources/UserResource.java
+++ src/main/java/com/clobovia/course/resources/UserResource.java
@@ -1,22 +1,38 @@
 package com.clobovia.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.clobovia.course.entities.User;
+import com.clobovia.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service; 
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
+	public ResponseEntity<List<User>> findAll(){
 		
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "CEINCECE");
-		return ResponseEntity.ok().body(u);
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 		
 	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	
 
 }
